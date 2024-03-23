5978623eedeff197af6237d750ae39eec6becccd
Alexandra <chshuk@mail.ru>
2024-03-24 00:20:15 +0300

    Задача решена

---
+++ decision/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] arrayStr = {"Hello","Rus", "'Russia'", "oct", ";-)", "combo", "min"};
+
+List<string> new_array = new List<string>();
+
+for(int i = 0; i < arrayStr.Length; i++)
+{
+    if (arrayStr[i].Length <= 3)
+    {
+        new_array.Add(arrayStr[i]);
+    }
+}
+
+foreach (string Element in new_array)
+{
+    Console.Write(Element + ',');
+}
